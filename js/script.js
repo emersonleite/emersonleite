2972fd395d2ff355e79f9f485f6f086f154edf9b
Emerson Leite <emerson@unna.eng.br>
2020-02-04 10:42:58 -0300

    202002041040

--- js/script.js
+++ js/script.js
@@ -4,11 +4,14 @@ import Element from "./module/construtor.js";
 import shine from "./module/shine.js";
 import tooltip from "./module/tooltip-foto.js";
 import tooltipCheat from "./module/tooltip-cheat.js";
+import Countdown from "./module/countdown";
 
 // Chamada das funções
-  accordion();
-  modal();
-  shine();
-  tooltip();
-  tooltipCheat();
+accordion();
+modal();
+shine();
+tooltip();
+tooltipCheat();
 
+const inicioSite = new Countdown("29 February 2020 23:59:59 GMT-0300");
+console.log(inicioSite);
