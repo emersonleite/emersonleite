56b98da586b3a651865799eba26214a0abc31e19
Emerson Leite <emerson@unna.eng.br>
2020-02-04 21:23:23 -0300

    202002042123-counter

---
+++ js/script.js
@@ -0,0 +1,31 @@
+import Countdown from "./countdown.js";
+
+const timeOnSite = new Countdown("27 February 2020 00:00:00 GMT-0300");
+const p = document.querySelector(".countdown p");
+const regexp = new RegExp(/(\d{2,})/g);
+
+console.log(timeOnSite.total);
+
+renderTime(timeOnSite);
+
+function format(text) {
+  if (!text.match(regexp)) {
+    return text.replace(/(\d)/, "0$1");
+  } else {
+    return text;
+  }
+}
+
+function renderTime(timeEntry) {
+  const t = timeEntry.total;
+  const timeOnPage =
+    format(`${t.days} dias, `) +
+    format(`${t.hours}`) +
+    ":" +
+    format(`${t.minutes}`) +
+    ":" +
+    format(`${t.seconds}`);
+  p.innerText = timeOnPage;
+}
+
+const timer = setInterval(renderTime, 1000, timeOnSite);
