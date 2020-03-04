bcbaa6635f9e92bd351dba676c321ecfecff3b84
Emerson Leite <emerson.bl@gmail.com>
2020-03-04 10:20:47 -0300

    202003041020

---
+++ docs/tmseditor/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    script: ['@babel/polyfill', 'whatwg-fetch', "./webpack/script.js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./js"),
+    filename: "[name].js"
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query: {
+          presets: ["@babel/preset-env"]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
